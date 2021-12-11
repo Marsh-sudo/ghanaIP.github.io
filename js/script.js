f4bef5e26d6f6f78f953bd9bfee845cd8304ebcd
Marsh-sudo <kelvinmunyao01@gmail.com>
2021-12-12 02:14:23 +0300

    Add arrays to the js file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var date = 31;
+if (date > 31) {
+    console.log("Invalid date");
+}
+ var month = 12;
+ if (month > 12){
+     console.log('Invalid month');
+ }
+ const maleNames = [Sunday["Kwasi"], Monday["Kwadwo"] ,Tuesday["Kwabena"] ,Wednesday["Kwaku"] ,Thursday["Yaw"], Friday["Kofi"] ,Saturday["Kwame"]]
