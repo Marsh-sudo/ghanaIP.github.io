57d605def276d1e90acde87424d40e84cfe7c1dd
Marsh-sudo <kelvinmunyao01@gmail.com>
2021-12-12 03:59:36 +0300

    Add else & if to js folder

--- js/script.js
+++ js/script.js
@@ -6,4 +6,23 @@ if (date > 31) {
  if (month > 12){
      console.log('Invalid month');
  }
- const maleNames = [Sunday["Kwasi"], Monday["Kwadwo"] ,Tuesday["Kwabena"] ,Wednesday["Kwaku"] ,Thursday["Yaw"], Friday["Kofi"] ,Saturday["Kwame"]]
+ const maleNames = [Sunday["Kwasi"], Monday["Kwadwo"] ,Tuesday["Kwabena"] ,Wednesday["Kwaku"] ,Thursday["Yaw"], Friday["Kofi"] ,Saturday["Kwame"]];
+ function let(male);{
+ let male = maleNames[1,2,3,4,5,6,7];
+ if (male === 1) {
+    console.log("You are Kwasi.")
+ } else if (male === 2) {
+     console.log("You are kwado")
+ }else if (male===3){
+     console.log("You are Kwabena")
+ }else if(male ===4){
+     console.log("You are Kwaku")
+ }else if(male ==5){
+     console.log("You are Yaw")
+ }else if(male===6){
+     console.log("You are Kofi")
+ }else if(male===7){
+     console.log("You are Kwame")
+ }
+ 
+}
