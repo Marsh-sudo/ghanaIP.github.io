a24c6da073ad26f6d3c97191f5ced9f16e3a999e
Marsh-sudo <kelvinmunyao01@gmail.com>
2021-12-13 04:10:38 +0300

    Add changes to male section

--- js/script.js
+++ js/script.js
@@ -26,24 +26,24 @@ function find_leapyear(){
 
  const maleNames = [Sunday["Kwasi"], Monday["Kwadwo"] ,Tuesday["Kwabena"] ,Wednesday["Kwaku"] ,Thursday["Yaw"], Friday["Kofi"] ,Saturday["Kwame"]];
  function let(male);{
- let male = maleNames[1,2,3,4,5,6,7];
- if (male === 1) {
+    let male = maleNames[1,2,3,4,5,6,7];
+   if (male === 1) {
     console.log("You are Kwasi.")
- } else if (male === 2) {
+   } else if (male === 2) {
      console.log("You are kwado")
- }else if (male===3){
+   }else if (male===3){
      console.log("You are Kwabena")
- }else if(male ===4){
+   }else if(male ===4){
      console.log("You are Kwaku")
- }else if(male ==5){
+   }else if(male ==5){
      console.log("You are Yaw")
- }else if(male===6){
+   }else if(male===6){
      console.log("You are Kofi")
- }else if(male===7){
+   }else if(male===7){
      console.log("You are Kwame")
- }else{
+   }else{
      console.log("Enter valid Day")
- }
+   }
  
 }
 
