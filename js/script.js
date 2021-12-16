5cbb51a0cbbd41570e6dc27ceb5438dbbcd99b6b
Marsh-sudo <kelvinmunyao01@gmail.com>
2021-12-16 17:00:39 +0300

    Add change of console.log to alert

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
-function getBirthday(){
-  document.getElementById
-  var date ="31"
-if (date > 31) {
-    console.log("Invalid date");
-}
- var month = 12;
- if (month > 12){
-     console.log('Invalid month');
- }
+
+function getAkaname(){
+
+  function getBirthday(){
+    document.getElementById
+    var date ="31"
+  if (date > 31) {
+      console.log("Invalid date");
+  }
+   var month = 12;
+   if (month > 12){
+       console.log('Invalid month');
+   }
+  
+     
 
 
- } 
-
-
-
-
-
- 
-
- 
-function find_leapyear(){
+  
+   } 
+   function find_leapyear(){
 
     var leapYear = document.getElementById("year").value;
 
@@ -30,51 +28,72 @@ function find_leapyear(){
     }
 
 }
+    
 
 
- const maleNames = [Sunday["Kwasi"], Monday["Kwadwo"] ,Tuesday["Kwabena"] ,Wednesday["Kwaku"] ,Thursday["Yaw"], Friday["Kofi"] ,Saturday["Kwame"]];
- function let(male);{
-     
+
+   const maleNames = [Sunday["Kwasi"], Monday["Kwadwo"] ,Tuesday["Kwabena"] ,Wednesday["Kwaku"] ,Thursday["Yaw"], Friday["Kofi"] ,Saturday["Kwame"]];
+   function let(male);{
     let male = maleNames[1,2,3,4,5,6,7];
-   if (male === 1) {
-    console.log("You are Kwasi.")
-   } else if (male === 2) {
-     console.log("You are kwado")
-   }else if (male===3){
-     console.log("You are Kwabena")
-   }else if(male ===4){
-     console.log("You are Kwaku")
-   }else if(male ==5){
-     console.log("You are Yaw")
-   }else if(male===6){
-     console.log("You are Kofi")
-   }else if(male===7){
-     console.log("You are Kwame")
-   }else{
-     console.log("Enter valid Day")
-   }
- 
-}
-
-const femaleNames = [Sunday["Akosua"],Monday["Adwoa"],Tuesday["Abonaa"],Wednesday["Akua"],Thursday["Yaa"], Friday["Afua"],Saturday["Ama"]];
-function let(female);{
-    var womaconst = femaleNames(1,2,3,4,5,6,7);
-    if (womanconst == 1){
-        console.log;("You are Akosua")
-    } else if (womaconst == 2) {
-        console.log("You are Adwoa")
-    }else if (womaconst == 3){
-        console.log("You are Abonaa")
-    }else if (womaconst == 4){
-        console.log("You are Akua")
-    }else if (womaconst == 5){
-        console.log("You are Yaa")
-    }else if (womaconst === 6){
-        console.log("You are Afua")
-    }else if (womaconst == 7){
-        console.log("You are Ama")
+    if (male === 1) {
+     alert("You are Kwasi.")
+    } else if (male === 2) {
+      alert("You are kwado")
+    }else if (male===3){
+      alert("You are Kwabena")
+    }else if(male ===4){
+      alert("You are Kwaku")
+    }else if(male ==5){
+      alert("You are Yaw")
+    }else if(male===6){
+      alert("You are Kofi")
+    }else if(male===7){
+      alert("You are Kwame")
     }else{
-        console.log("Enter valid Date")
+      alert("Enter valid Day")
     }
-}
+  
+    const femaleNames = [Sunday["Akosua"],Monday["Adwoa"],Tuesday["Abonaa"],Wednesday["Akua"],Thursday["Yaa"], Friday["Afua"],Saturday["Ama"]];
+    function let(female);{
+        var womaconst = femaleNames(1,2,3,4,5,6,7);
+        if (womanconst == 1){
+            alert("You are Akosua")
+        } else if (womaconst == 2) {
+            alert("You are Adwoa")
+        }else if (womaconst == 3){
+            alert("You are Abonaa")
+        }else if (womaconst == 4){
+            alert("You are Akua")
+        }else if (womaconst == 5){
+            alert("You are Yaa")
+        }else if (womaconst === 6){
+            alert("You are Afua")
+        }else if (womaconst == 7){
+            alert("You are Ama")
+        }else{
+            alert("Enter valid Date")
+        }
+     }
+    
+    event.preventDefault()
+
+
+
+ }
+
+ 
+
+
+
+
+
+ 
+
+ 
+
+
+
+
+
+ 
 
