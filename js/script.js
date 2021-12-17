04b49f5b53265262129441dd8ff3a3a7eee31335
Marsh-sudo <kelvinmunyao01@gmail.com>
2021-12-17 17:29:40 +0300

    Add multiple changes to script.js

--- js/script.js
+++ js/script.js
@@ -1,99 +1,62 @@
-
-function getAkaname(){
-
-  function getBirthday(){
-    document.getElementById
-    var date ="31"
+$(document).ready(function()
+$(form).submit(function(event){
+  var date = "31"
   if (date > 31) {
-      console.log("Invalid date");
-  }
-   var month = 12;
-   if (month > 12){
-       console.log('Invalid month');
-   }
-  
-     
-
-
-  
-   } 
-   function find_leapyear(){
-
-    var leapYear = document.getElementById("year").value;
-
-    if((leapYear % 4 == 0) && (leapYear % 100 !=0) || (leapYear % 400 == 0)) {
-        alert(leapYear + "True")
-    }else{
-        alert(leapYear + "False")
-    }
-
+    console.log("Invalid date");
 }
-    
-
-
-
-   const maleNames = [Sunday["Kwasi"], Monday["Kwadwo"] ,Tuesday["Kwabena"] ,Wednesday["Kwaku"] ,Thursday["Yaw"], Friday["Kofi"] ,Saturday["Kwame"]];
-   function let(male);{
-    let male = maleNames[1,2,3,4,5,6,7];
-    if (male === 1) {
-     alert("You are Kwasi.")
-    } else if (male === 2) {
-      alert("You are kwado")
-    }else if (male===3){
-      alert("You are Kwabena")
-    }else if(male ===4){
-      alert("You are Kwaku")
-    }else if(male ==5){
-      alert("You are Yaw")
-    }else if(male===6){
-      alert("You are Kofi")
-    }else if(male===7){
-      alert("You are Kwame")
-    }else{
-      alert("Enter valid Day")
-    }
+var month = 12;
+if (month > 12){
+    console.log('Invalid month');
+}
   
-    const femaleNames = [Sunday["Akosua"],Monday["Adwoa"],Tuesday["Abonaa"],Wednesday["Akua"],Thursday["Yaa"], Friday["Afua"],Saturday["Ama"]];
-    function let(female);{
-        var womaconst = femaleNames(1,2,3,4,5,6,7);
-        if (womanconst == 1){
-            alert("You are Akosua")
-        } else if (womaconst == 2) {
-            alert("You are Adwoa")
-        }else if (womaconst == 3){
-            alert("You are Abonaa")
-        }else if (womaconst == 4){
-            alert("You are Akua")
-        }else if (womaconst == 5){
-            alert("You are Yaa")
-        }else if (womaconst === 6){
-            alert("You are Afua")
-        }else if (womaconst == 7){
-            alert("You are Ama")
-        }else{
-            alert("Enter valid Date")
-        }
+
+     
+      var days = document.getElementById("gender").value;
+     var days = function(Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday)
+     var male = (Kwasi,Kwado,Kwabena,Kwaku,Yaw,Kofi,Kwame);
+     function msgPrint(male){
+      if (days == Sunday && male == Kwasi) {
+        alert("You are Kwasi.")
+       } else if (days== Monday && male == kwado) {
+         alert("You are kwado")
+       }else if(days == Tuesday && male == Kwabena){
+         alert("You are Kwabena")
+       }else if(days == Wednesday && male == Kwaku){
+         alert("You are Kwaku")
+       }else if(days == Thursday && male == Yaw){
+         alert("You are Yaw")
+       }else if(days == Friday && male == Kofi){
+         alert("You are Kofi")
+       }else if(days == Saturday && male == Kwame){
+         alert("You are Kwame")
+       }else{
+         alert("Enter valid Day")
+       }
      }
     
+    var days = document.getElementById("gender").value;
+    var days = function(Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday) ;
+    var femaleNames = (Akosua,Adowoa,Abenaa,Akua,Yaa,Afua,Yaa,Afua,Ama)
+      function msgPrint(femaleNames){
+        if (days == Sunday && femaleNames == Akosua){
+          alert("You are Akosua")
+      } else if (days == Monday && femaleNames == Adowoa) {
+          alert("You are Adwoa")
+      }else if (days == Tuesday && femaleNames == Abenaa){
+          alert("You are Abonaa")
+      }else if (days == Wednesday && femaleNames == Akua){
+          alert("You are Akua")
+      }else if (days == Thursday && femaleNames == Yaa){
+          alert("You are Yaa")
+      }else if (days == Friday && femaleNames == Afua){
+          alert("You are Afua")
+      }else if (days == Saturday && femaleNames == Ama){
+          alert("You are Ama")
+      }else{
+          alert("Enter valid Date")
+      }
+      }
+        
     event.preventDefault()
 
-
-
- }
-
- 
-
-
-
-
-
- 
-
- 
-
-
-
-
-
- 
-
+    });
