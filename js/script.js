67eb04d1603770b2327d5045d3b98fcc3b0cadb8
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-01-13 11:42:20 +0300

    Added if and else

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function getValueAkan() {
+function getValueAkan(){
   var date = document.getElementById('date').value;
   var month = document.getElementById('month').value;
   var year = document.getElementById('year').value;
@@ -25,77 +25,64 @@ function validateValues(date,month,year,gender){
       getDayOfWeek(gender,century,year,month,day);
   
 
-}
+};
 
-function getDayOfWeek(gender,century,year,month,day);
-    //use formulae on Ip
-    var CC = parseInt(document.getElementById("century").value);
-    var MM = parseInt(document.getElementById("month").value);
-    var DD = parseInt(document.getElementById("day").value);
-    var YY = parseInt(document.getElementById("year").value);
+function getDayOfWeek(gender,century,year,month,day){
+  //use formulae on Ip
+  var CC = parseInt(document.getElementById("century").value);
+  var MM = parseInt(document.getElementById("month").value);
+  var DD = parseInt(document.getElementById("day").value);
+  var YY = parseInt(document.getElementById("year").value);
+  
+  var century = year.slice(0,1);
+  var year = year.slice(2,3);
+  
+
+
+  // use fomula
+
+  var dayOfWeek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+  var dw = Math.floor(dayOfWeek);
+ 
+   getAkanNames(dw);
+
+};
     
-    var century = year.slice(0,1);
-    var year = year.slice(2,3);
-    
-
-
-    // use fomula
-
-    var dayOfWeek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-    var dw = Math.floor(dayOfWeek);
-   
-     getAkanNames(dw);
 
   
 function getAkanNames(dw){
      var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+     var femaleNames = ['Akosua','Akwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+     var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
      var newAnswer = dw.toString() + gender;
-     switch (newAnswer) {
-       case "1Male":
-         alert("Your name is Kwasi,you were born on" + days[1]);
-         break;
-         case "1female":
-           alert("You are Akosua,you were born on" + days[1]);
-           break;
-           case "2Male":
-             alert("You Akan Kwadwo,you were born on" + days[2]);
-             break;
-             case "2Female":
-               alert("Your Akan is Adwoa,you were born on" + days[2]);
-               break;
-               case "3Male":
-                 alert("You are is kwabena,you were born on" + days[3]);
-                 break;
-                 case "3Female":
-                   alert("You are is Akua,you were born on" + days[3]);
-                   break;
-                   case "4Male":
-                     alert("You are is Kwaku,you were born" + days[4]);
-                     break;
-                     case "4Female":
-                       alert("You are  Yaa,you were born on" + days[4]);
-                       break;
-                       case "5Male":
-                         alert("Your are Yaw,you were born on" + days[5]);
-                         break;
-                         case "5Female":
-                           alert("Your are Afua,you were born on" + days[5]);
-                           break;
-                           case "6Male":
-                             alert("Your are Kofi,you were born on" + days[6]);
-                             break;
-                             case "6Female":
-                               alert(" Your are Ama,you were born on" + days[6]);
-                               break;
-                               case "7Male":
-                                 alert("Your are Kwame,you were born on" + days[7]);
-                                 break;
-                                 case "7Female":
-                                   alert("Your are Amani,you were born on" + days[7]);
-                                 break;
-                                 default:
-                                   alert("Sorry no such Entry")
+     if (days == 1 && gender === 'female'){
+       alert("You are" + femaleNames[0]);
+     }else if(days == 1 && gender === 'male'){
+       alert("You are" + maleNames[0]);
+     }else if(days == 2 && gender === 'female'){
+       alert("You are" + femaleNames[1]);
+     }else if(days == 2 && gender === 'male'){
+       alert("You are" + maleNames[1]);
+     }else if(days == 3 && gender === 'female'){
+       alert("You are" + femaleNames[2]);
+     }else if(days == 3 && gender === 'male'){
+       alert("You are" + maleNames[2]);
+     }else if(days == 4 && gender == 'female'){
+       alert("You are" + femaleNames[3]);
+     }else if(days == 4 && gender === 'male'){
+       alert("You are" + maleNames[3]);
+     }else if(days == 5 && gender == 'female'){
+       alert("You are" + femaleNames[4]);
+     }else if(days == 5 && gender == 'male'){
+       alert("You are" + maleNames[4]);
+     }else if(days == 6 && gender == 'female'){
+       alert("You are" + femaleNames[5]);
+     }else if(days == 6 && gender == 'male'){
+       alert("You are" + maleNames[5]);
+     }else if(days == 7 && gender == 'female'){
+       alert(" You are" + femaleNames[6]);
+     }else if(days == 7 && gender == 'male'){
+       alert("You are" + maleNames[6]);
      }
-
  }
 };
