efcb297c71e5cfdb61c7598331994798eab8be2e
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-01-17 18:58:39 +0300

    Change of formula used to calculate day of week

--- js/script.js
+++ js/script.js
@@ -7,78 +7,81 @@ function getValueAkan(){
   //convert to integers
   var monthInt = parseInt(monthOfBirth);
   var dateInt = parseInt(dateOfBirth);
-  var century = yearOfBirth.slice(0,2);
-  var bornYear = yearOfBirth.slice(2,5);
-  var CC = parseInt(century);
-  var YY = parseInt(bornYear);
-  console.log(century + " " + bornYear)
-  validateValues(dateInt,monthInt,CC,YY,gender);
+  var yearInt = parseInt(yearOfBirth);
+  
+  validateValues(dateInt,monthInt,gender,yearInt);
 };
 
 
 
-function validateValues(dateInt,monthInt,gender,YY,CC){
+function validateValues(dateInt,monthInt,gender,yearInt){
     //parseInt
     
     if(dateInt > 31 || dateInt == 0){
       alert("error in date entry");
     } else if(dateInt == " "){
       alert("date cannot be empty");
-    } if(monthInt > 12 || monthInt == 0){
+    }else if(monthInt > 12 || monthInt == 0){
       alert("error in month entry");
     }else{
-      getDayOfWeek(gender,CC,YY,monthInt,dateInt);
-  
+      
     }
-  
+
+   dayOfWeek(dateInt,monthInt,gender,yearInt)
 
 };
 
-function getDayOfWeek(gender,CC,YY,monthInt,dateInt){
+
   //use formulae on Ip
-  console.log(CC + " " + YY);
+  function dayOfWeek(dateInt,monthInt,gender,yearInt){
+    //get the exact day in the week
+      //magic number month array
+      let t = [ 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 ];
+      yearInt -= (monthInt < 3) ? 1 : 0;
+      //sakamoto formula (Time Complexity)
+      var dw =  ( yearInt + yearInt/4 - yearInt/100 + yearInt/400 + t[monthInt-1] + dateInt) % 7;
+      //truncates decimal part
+      var newDw = Math.trunc(dw);
+      console.log(newDw);
+      finalOutput(newDw,gender)
+  
+  }
+   
 
-  // use fomula
-
-  var dayOfWeek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(monthInt+1)/10)) + dateInt ) % 7;
-  var dw = Math.floor(dayOfWeek);
- 
-   getAkanNames(dw,gender);
-
-};
+;
     
  
-function getAkanNames(dw,gender){
+function finalOutput(newDw,gender){
      var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
      var femaleNames = ['Akosua','Akwoa','Abenaa','Akua','Yaa','Afua','Ama'];
      var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-     if(dw == 1 && gender === 'male'){
+     if(newDw == 0 && gender === 'male'){
        alert("You were born on" + days[0] + "Your akan name is" + maleNames[0]);
-     }else if(dw == 1 && gender === 'female'){
+     }else if(newDw == 0 && gender === 'female'){
        alert("You were born on" + days[0] + "Your akan name is" + femaleNames[0]);
-     }else if(dw == 2 && gender === 'male'){
+     }else if(newDw == 1 && gender === 'male'){
        alert("You were born on" + days[1] + "Your akan name is" + maleNames[1]);
-     }else if (dw == 2 && gender === 'female'){
+     }else if (newDw == 1 && gender === 'female'){
        alert("You were born on" + days[1] + "Your akan name is" + femaleNames[1]);
-     }else if(dw == 3 && gender === 'male'){
+     }else if(newDw == 2 && gender === 'male'){
        alert("You were born on" + days[2] + "Your akan name is" + maleNames[2]);
-     }else if(dw == 3 && gender === 'female'){
+     }else if(newDw == 2 && gender === 'female'){
        alert("You were born on" + days[2] + "Your akan name is" + femaleNames[2]);
-     }else if(dw == 4 && gender === 'male'){
+     }else if(newDw == 3 && gender === 'male'){
        alert("You were born on" + days[3] + "Your akan name is" + maleNames[3]);
-     }else if(dw == 4 && gender === 'female'){
+     }else if(newDw == 3 && gender === 'female'){
        alert("You were born on" + days[3] + "Your akan name is" + femaleNames[3]);
-     }else if(dw == 5 && gender === 'male'){
+     }else if(newDw == 4 && gender === 'male'){
        alert("You were born on" + days[4] + "Your akan name is" + maleNames[4]);
-     }else if(dw == 5 && gender === 'female'){
+     }else if(newDw == 4 && gender === 'female'){
        alert("You were born on" + days[4] + "Your akan name is" + femaleNames[4]);
-     }else if(dw == 6 && gender === 'male'){
+     }else if(newDw == 5 && gender === 'male'){
        alert("You were born on" + days[5] + "Your Akan name is" + maleNames[5]);
-     }else if(dw == 6 && gender === 'female'){
+     }else if(newDw == 5 && gender === 'female'){
        alert("You were born on" + days[5] + "Your akan name is" + femaleNames[5]);
-     }else if(dw == 7 && gender === 'male'){
+     }else if(newDw == 6 && gender === 'male'){
        alert("You were born on" + days[6] + "Your akan name is" + maleNames[6]);
-     }else if(dw == 7 && gender === 'female'){
+     }else if(newDw == 6 && gender === 'female'){
        alert("You were born on" + days[6] + "Your akan name is" + femaleNames[6]);
      }else{
        alert("OOOPS!!SOMETHING WENT WRONG :-(")
